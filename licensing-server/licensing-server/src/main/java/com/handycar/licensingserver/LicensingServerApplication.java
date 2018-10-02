708442a8363f77057d760d49269bad167ddc6dbc
GAIN LEE <dlrkdls91@naver.com>
2018-10-02 16:13:01 +0900

    feign client, hystrix 적용

--- licensing-server/licensing-server/src/main/java/com/handycar/licensingserver/LicensingServerApplication.java
+++ licensing-server/licensing-server/src/main/java/com/handycar/licensingserver/LicensingServerApplication.java
@@ -2,11 +2,15 @@ package com.handycar.licensingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableCircuitBreaker
 public class LicensingServerApplication {
 
     @Bean
--- licensing-server/licensing-server/src/main/java/com/handycar/licensingserver/services/LicenseService.java
+++ licensing-server/licensing-server/src/main/java/com/handycar/licensingserver/services/LicenseService.java
@@ -1,14 +1,17 @@
 package com.handycar.licensingserver.services;
 
-import com.handycar.licensingserver.clients.OrganizationRestTemplateClient;
+import com.handycar.licensingserver.clients.OrganizationFeignClient;
 import com.handycar.licensingserver.config.ServiceConfig;
 import com.handycar.licensingserver.model.License;
 import com.handycar.licensingserver.model.Organization;
 import com.handycar.licensingserver.repository.LicenseRepository;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 
 @Service
@@ -18,13 +21,14 @@ public class LicenseService {
     private LicenseRepository licenseRepository;
 
     @Autowired
-    private OrganizationRestTemplateClient organizationRestClient;
+    OrganizationFeignClient organizationFeignClient;
 
     @Autowired
     ServiceConfig config;
 
+    @HystrixCommand
     public License getLicense(String organizationId,String licenseId) {
-        Organization organization = organizationRestClient.getOrganizaion(organizationId);
+        Organization organization = organizationFeignClient.getOrganization(organizationId);
 
         License license = licenseRepository.findByOrganizationIdAndLicenseId(organizationId, licenseId);
         return license
@@ -35,8 +39,30 @@ public class LicenseService {
                 .withComment(config.getExampleProperty());
     }
 
+    @HystrixCommand
     public List<License> getLicensesByOrg(String organizationId){
+        randomlyRunLong();
+
         return licenseRepository.findByOrganizationId( organizationId );
+    }
+
+    private void randomlyRunLong() {
+        Random rand = new Random();
+
+        int randomNum = rand.nextInt(3) + 1;
+
+        if(randomNum == 3){
+            System.out.println("Hit as delayed service");
+            sleep();
+        }
+    }
+
+    private void sleep() {
+        try{
+            Thread.sleep(5000);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
     }
 
     public void saveLicense(License license){
